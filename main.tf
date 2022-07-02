4febc8073c8017b20fd2b0184e793e79bafbdcd2
yuizho <yuizho3@gmail.com>
2022-07-02 16:09:43 +0900

    modify main.tf to add sg config

--- main.tf
+++ main.tf
@@ -6,9 +6,28 @@ variable "example_instance_type" {
   default = "t3.micro"
 }
 
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0b7546e839d7ace12"
-  instance_type = var.example_instance_type
+  ami                    = "ami-0b7546e839d7ace12"
+  instance_type          = var.example_instance_type
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
   tags = {
     Name = "example"
