ad8835c4188f26b87a6cac7bf90a0c7eb009baf7
yuizho <yuizho3@gmail.com>
2022-07-02 15:47:50 +0900

    modify main.tf to add variable

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0b7546e839d7ace12"
-  instance_type = "t3.micro"
+  instance_type = var.example_instance_type
 
   tags = {
     Name = "example"
